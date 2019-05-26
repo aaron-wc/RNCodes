3019e3b54aba8282304f1b00f2b23a5dc8f857e3
aaron-wc <likeclem30@gmail.com>
2019-05-26 21:22:09 +0100

    fetch-1

--- microapp/App.js
+++ microapp/App.js
@@ -1,24 +1,40 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet,Button, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {text:'Welcome to React Native!', desc:'To Get Started edit App',item:''}
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Button title='Get Data' onPress={this._getData}/>
+        <Text style={styles.welcome} >{this.state.text}</Text>
+        <Text style={styles.welcome}>{this.state.desc}</Text>
+        <Text style={styles.welcome}>{this.state.item}</Text>
+        
       </View>
     );
+  }
+  _getData = async () =>{
+
+    fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        text:responseJson.title,
+        desc:responseJson.description,
+        item:responseJson.movies[2].title
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 }
 
@@ -34,9 +50,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
